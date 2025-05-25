1cdc3e61a868bd859bc641930c873836cb0cbc37
BrightSparkle <ekaterinamrch@gmail.com>
2025-05-25 17:50:12 +0300

    Change main

--- Reflection/src/main/java/org/example/Main.java
+++ Reflection/src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.io.*;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SomeBean sb = null;
+        try {
+            sb = new Injector().inject(new SomeBean());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        sb.foo();
     }
-
 }
