a1be7385022bcadfe42336b8f6db53d2a3dd6845
BrightSparkle <ekaterinamrch@gmail.com>
2025-05-25 17:42:47 +0300

    Create class Injector

---
+++ Reflection/src/main/java/org/example/Injector.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.lang.reflect.*;
+import java.io.*;
+import java.util.Properties;
+
+public class Injector {
+    private final Properties properties;
+
+    public Injector() throws IOException {
+        properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new FileNotFoundException("Файл config.properties не найден");
+            }
+            properties.load(input);
+        }
+    }
+
+    public <T> T inject(T obj) throws Exception {
+        // Перебираем все поля объекта
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(AutoInjectable.class)) {
+                // Получаем тип поля (интерфейс)
+                Class<?> fieldType = field.getType();
+
+                // Ищем реализацию в properties
+                String implClassName = properties.getProperty(fieldType.getName());
+                if (implClassName == null) {
+                    throw new RuntimeException("Реализация для " + fieldType.getName() + " не найдена");
+                }
+
+                // Создаем экземпляр класса реализации
+                Class<?> implClass = Class.forName(implClassName);
+                Object implInstance = implClass.getDeclaredConstructor().newInstance();
+
+                // Устанавливаем значение поля
+                field.setAccessible(true);
+                field.set(obj, implInstance);
+            }
+        }
+        return obj;
+    }
+}
